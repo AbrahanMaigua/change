ccc177d12393670534327f6f11abc6b0b6f3a91b
Abrahan Maigua <abrahanmaigua15@gmail.com>
2024-03-18 14:58:09 +0000

    fix bug

--- libs/db.py
+++ libs/db.py
@@ -25,7 +25,7 @@ def create_pedido(date, fomatado, seg, valor):
 
     sql = '''
     INSERT INTO pedido(created_at, tiempo_carga, valor, segundo_total)
-      VALUES (TO_DATE(%s, 'MM/DD/YYYY'), %s, %s, %s)  SET datestyle = mdy;
+      VALUES (%s, %s, %s, %s);
         
     '''
     conn = create_connection()
