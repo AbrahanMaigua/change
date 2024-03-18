a6f5e055d0a195cd921fc9dc4cfe283b18e7e32f
Abrahan Maigua <abrahanmaigua15@gmail.com>
2024-03-18 14:33:42 +0000

    comit

--- libs/db.py
+++ libs/db.py
@@ -25,7 +25,7 @@ def create_pedido(date, fomatado, seg, valor):
 
     sql = '''
     INSERT INTO pedido(created_at, tiempo_carga, valor, segundo_total)
-      VALUES (%s, %s, %s, %s)
+      VALUES (%s, %s, %s, %s)  SET datestyle = mdy;
         
     '''
     conn = create_connection()
